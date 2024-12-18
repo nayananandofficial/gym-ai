ddd8b7582b0b41b5aaed422ff26a509e2c066e34
Nayan <nayan@gmail.com>
2024-12-18 17:38:19 +0530

    video file name changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import ArrowButton from "../components/ArrowButton";
-import FullVideo from '../assets/full-video.mp4';
+import fullvideo from '../assets/full-video.mp4';
 
 
 const Home = () => {
@@ -10,7 +10,7 @@ const Home = () => {
     return(
 
         <section id="home" className="h-screen w-full -z-10">
-            <video className="absolute top-0 left-0 w-full h-full object-cover -z-10" src={FullVideo} autoPlay loop muted></video>
+            <video className="absolute top-0 left-0 w-full h-full object-cover -z-10" src={fullvideo} autoPlay loop muted></video>
             <Header />
             <ArrowButton href="#about"/>
         </section>
